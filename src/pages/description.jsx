8b4266fb1d9b7146bb7f7caba036b85d97d5ec2d
Jawed-firdous <134147168+Jawed-firdous@users.noreply.github.com>
2023-06-27 05:38:57 -0700

    Update description.jsx

--- src/pages/description.jsx
+++ src/pages/description.jsx
@@ -418,6 +418,8 @@ function DescriptionPage() {
     useEffect(() => {
         getUserData()
         updateProductInLocalStorage()
+            const url = window.location.href;
+
     },
 
               [])
@@ -519,6 +521,7 @@ function DescriptionPage() {
                                     <a class="ml-3 text-gray-500" href="https://www.youtube.com/@sukoondiabetescentre1989/videos" target="_blank">
                                         <FaYoutube />
                                     </a>
+                                    
                                     <a class="ml-3 text-gray-500" href="https://wa.me/+923322418007?text={url}" target="_blank">
                                         <FaWhatsapp />
                                     </a>
