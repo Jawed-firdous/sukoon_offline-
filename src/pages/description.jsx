d60438a3db2d02be36aa36a105e97dc7968ccb1a
Jawed-firdous <134147168+Jawed-firdous@users.noreply.github.com>
2023-07-12 02:46:52 -0700

    Update description.jsx

--- src/pages/description.jsx
+++ src/pages/description.jsx
@@ -421,7 +421,7 @@ function DescriptionPage() {
         updateProductInLocalStorage()
         var Id = `${router?.asPath} `;
 switch ( Id ) { 
-	case 'productId=7zG0TGyVfvJ2CBweAnGa': 
+	case 'productId=/description?productId=7zG0TGyVfvJ2CBweAnGa': 
 		alert('jQuery sucks!');
 		break;
 	case 'prototype': 
