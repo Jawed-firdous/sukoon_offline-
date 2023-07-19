7d2a1889bf935c9bd99fb2eb8c679d8db9677eff
Jawed-firdous <134147168+Jawed-firdous@users.noreply.github.com>
2023-07-19 00:58:52 -0700

    Update description.jsx

--- src/pages/description.jsx
+++ src/pages/description.jsx
@@ -426,8 +426,8 @@ function DescriptionPage() {
 switch (`${router?.asPath}`) { 
 	case '/description?productId=7zG0TGyVfvJ2CBweAnGa': 
 		var Pro = "https://click.daraz.pk/e/_CWG1y9" ;
-		alert(` Pro `);
-		//return "Pro";
+		//alert(` Pro `);
+		return "Pro";
 		break;
 	case 'prototype': 
 		alert('prototype sucks!');
