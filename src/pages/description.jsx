dd12f67f5fccecc73e9dc9292f4e52421f061a5e
Jawed-firdous <134147168+Jawed-firdous@users.noreply.github.com>
2023-07-26 02:50:06 -0700

    Update description.jsx

--- src/pages/description.jsx
+++ src/pages/description.jsx
@@ -428,9 +428,7 @@ switch (`${router?.asPath}`) {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  
 ); 
 		alert(` Pro `);
 		//return "Pro";
