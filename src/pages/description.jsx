90a4f021e4baf13b9e66aba8a2a19959bd12173a
Jawed-firdous <134147168+Jawed-firdous@users.noreply.github.com>
2023-07-26 02:34:23 -0700

    Update description.jsx

--- src/pages/description.jsx
+++ src/pages/description.jsx
@@ -530,11 +530,31 @@ switch (`${router?.asPath}`) {
                                     </div>
                                     <button className="bg-[#94c755] text-white px-3 py-2 rounded"> {
                                         checkIdExists(productId) ? "Added to cart" : <span onClick={() => addToCart(productId, "plus")}>Add to cart</span>
-                                    }</button> 
-
-				<button className="bg-[#94c755] text-white px-3 py-2 rounded">
+                                    }</button>
+					{
+						switch (`${router?.asPath}`) { 
+	case '/description?productId=7zG0TGyVfvJ2CBweAnGa': 
+		var Pro = "https://click.daraz.pk/e/_CWG1y9" ;
+									<button className="bg-[#94c755] text-white px-3 py-2 rounded">
                                     <a href="#" class="btn btn-primary btn-lg disabled" role="button" aria-disabled="true">Order To Daraz </a>
                                     </button>
+		alert(` Pro `);
+		//return "Pro";
+		break;
+	case 'prototype': 
+		alert('prototype sucks!');
+	case 'mootools': 
+		alert('mootools sucks!');	
+	case 'dojo': 
+		alert('dojo sucks!');
+		break;
+	default:
+		alert(`router.query.productId`);
+}
+
+					}
+
+
 
                                 </div>
 
