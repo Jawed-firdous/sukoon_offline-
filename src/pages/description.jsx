86b79193f48df0ec01cc9fb4b56bef4cf9e33ac4
Jawed-firdous <134147168+Jawed-firdous@users.noreply.github.com>
2023-07-26 01:57:52 -0700

    Update description.jsx

--- src/pages/description.jsx
+++ src/pages/description.jsx
@@ -420,7 +420,7 @@ function DescriptionPage() {
         getUserData()
         updateProductInLocalStorage()
 	  
-	if('${router?.asPath} == /description?productId=7zG0TGyVfvJ2CBweAnGa'){
+	if("${router?.asPath} == /description?productId=7zG0TGyVfvJ2CBweAnGa"){
 	    alert(' https://click.daraz.pk/e/_CWG1y9 ');
 	    }
 // switch (`${router?.asPath}`) { 
