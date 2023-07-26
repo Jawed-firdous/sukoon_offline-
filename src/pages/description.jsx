e86c3a204c2a98a662e526c28a0c4c39c84f6723
Jawed-firdous <134147168+Jawed-firdous@users.noreply.github.com>
2023-07-26 04:46:43 -0700

    Update description.jsx

--- src/pages/description.jsx
+++ src/pages/description.jsx
@@ -424,7 +424,7 @@ function DescriptionPage() {
 switch (`${router?.asPath}`) { 
 	case '/description?productId=7zG0TGyVfvJ2CBweAnGa': 
 		globalVariable = "https://click.daraz.pk/e/_CWG1y9" ;
-		alert(` console.log(globalVariable) `);
+		alert(`globalVariable`);
 		//return 'window.$name';
 		break;
 	case 'prototype': 
@@ -532,7 +532,7 @@ switch (`${router?.asPath}`) {
                                         checkIdExists(productId) ? "Added to cart" : <span onClick={() => addToCart(productId, "plus")}>Add to cart</span>
                                     }</button>
 			              <button className="bg-[#94c755] text-white px-3 py-2 rounded">
-                                    <a href="{console.log(globalVariable);}" class="btn btn-primary btn-lg disabled" role="button" aria-disabled="true">Order To Daraz </a>
+                                    <a href="{`globalVariable`}" class="btn btn-primary btn-lg disabled" role="button" aria-disabled="true">Order To Daraz </a>
                                     </button>
 
                                     </div>
