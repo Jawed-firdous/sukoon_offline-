d3c70ffce66f2487c64001b1101a99c3c936cf6e
Jawed-firdous <134147168+Jawed-firdous@users.noreply.github.com>
2023-08-01 05:38:31 -0700

    Update description.jsx

--- src/pages/description.jsx
+++ src/pages/description.jsx
@@ -541,7 +541,7 @@ function getDataFromSwitch(router) {
                                         checkIdExists(productId) ? "Added to cart" : <span onClick={() => addToCart(productId, "plus")}>Add to cart</span>
                                     }</button>
 	<button className="bg-[#94c755] text-white px-3 py-2 rounded">
-  <a href={console.log(result)} className="btn btn-primary btn-lg disabled" role="button" aria-disabled="true">
+  <a href={result} className="btn btn-primary btn-lg disabled" role="button" aria-disabled="true">
     Order To Daraz
   </a>
 </button>
