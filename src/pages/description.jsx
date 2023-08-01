810fa3823b81896fc00586392b063ccdacee56e5
Jawed-firdous <134147168+Jawed-firdous@users.noreply.github.com>
2023-08-01 06:24:53 -0700

    Update description.jsx

--- src/pages/description.jsx
+++ src/pages/description.jsx
@@ -541,7 +541,7 @@ const YourComponent = ({ router }) => {
                                         checkIdExists(productId) ? "Added to cart" : <span onClick={() => addToCart(productId, "plus")}>Add to cart</span>
                                     }</button>
 	<button className="bg-[#94c755] text-white px-3 py-2 rounded">
-      <a href={"result"} className="btn btn-primary btn-lg disabled" role="button" aria-disabled="true">
+      <a href={result} className="btn btn-primary btn-lg disabled" role="button" aria-disabled="true">
         Order To Daraz
       </a>
     </button>
@@ -772,6 +772,7 @@ const YourComponent = ({ router }) => {
         </>
     )
 }
+	      };
 export default YourComponent;
 
 export default DescriptionPage
