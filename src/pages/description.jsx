0f6d3e8159588434d05b54230fab2373debb2b65
Jawed-firdous <134147168+Jawed-firdous@users.noreply.github.com>
2023-08-02 02:21:54 -0700

    Update description.jsx

--- src/pages/description.jsx
+++ src/pages/description.jsx
@@ -27,7 +27,7 @@ import { signOut } from "firebase/auth";
 function DescriptionPage() {
     const router = useRouter()
     // const productId = router.query?.productId;
-	const message = null;
+	let message = null;
     const [productId, setProductId] = useState(null)
     const [data, setData] = useState([])
     const [showCart, setShowCart] = useState(false)
@@ -539,7 +539,7 @@ function getDataFromSwitch(router) {
                                         checkIdExists(productId) ? "Added to cart" : <span onClick={() => addToCart(productId, "plus")}>Add to cart</span>
                                     }</button>
 	<button className="bg-[#94c755] text-white px-3 py-2 rounded">
-      <a href={result} className="btn btn-primary btn-lg disabled" role="button" aria-disabled="true">
+      <a href={message} className="btn btn-primary btn-lg disabled" role="button" aria-disabled="true">
         Order To Daraz
       </a>
     </button>
