3629254db508ea31a7933f3ca9a0d3cd6d11cfac
Jawed-firdous <134147168+Jawed-firdous@users.noreply.github.com>
2023-08-04 05:34:00 -0700

    Update description.jsx

--- src/pages/description.jsx
+++ src/pages/description.jsx
@@ -418,12 +418,18 @@ function DescriptionPage() {
     useEffect(() => {
         getUserData()
         updateProductInLocalStorage()
-            if (router?.asPath == '/description?productId=7zG0TGyVfvJ2CBweAnGa'){
-                 let x ="https://click.daraz.pk/e/_CWG1y9"; 
-                                        alert( x );
+            // Assuming router is defined somewhere in your code or imported
+// Check if the router object exists and its asPath matches the specified path
+if (router?.asPath === '/description?productId=7zG0TGyVfvJ2CBweAnGa') {
+    // If the condition is true, set the value of variable 'x' to "https://click.daraz.pk/e/_CWG1y9"
+    let x = "https://click.daraz.pk/e/_CWG1y9";
+    // Display an alert with the value of 'x', which will show the URL "https://click.daraz.pk/e/_CWG1y9"
+    alert(x);
+} else {
+    // Handle the case when the condition is false or when router is not available
+    console.log("Router object not available or path does not match.");
+}
 
-
-            }
                
             
   //   switch (router?.asPath) {
