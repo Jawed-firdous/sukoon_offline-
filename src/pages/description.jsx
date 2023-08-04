be674edc02fc0d6575f01e5d574a1f5f258648fb
Jawed-firdous <134147168+Jawed-firdous@users.noreply.github.com>
2023-08-04 05:23:43 -0700

    Update description.jsx

--- src/pages/description.jsx
+++ src/pages/description.jsx
@@ -26,7 +26,7 @@ import Footer from "@/components/footer";
 import { signOut } from "firebase/auth";
 
 function DescriptionPage() {
-                       let x;
+                       let x="a";
     const router = useRouter()
     // const productId = router.query?.productId;
     const [productId, setProductId] = useState(null)
